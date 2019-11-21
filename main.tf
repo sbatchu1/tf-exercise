0002f5bd45464107910a3fe8af10a849c4d21062
sairamgopal.batchu <sairamgopal.batchu@sairamgopalbatchus-MacBook-Pro.local>
2019-11-21 11:31:41 +0530

    final commit

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
-
-
-variable "sai_msg"{
-    default = "default msg"
-}
-
 module "mukesh-ref" {
-    source = "github.com/mukesh4338/mukesh-tfrepo"
+  source = "github.com/mukesh4338/mukesh-tfrepo"
 }
 
 
 
-output "sai_msg"{
-    value = "${format("%s!, -this is sai's message", module.mukesh-ref.result_mukesh)}"
+output "sai_msg" {
+  value = "${format("%s!, -this is sai's message", module.mukesh-ref.result_mukesh)}"
 }
